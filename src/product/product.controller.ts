61f4cd833ba87d13bfb6b06a0a02c6332006d5df
Agustin Gomez <agusleongomez@gmail.com>
2022-11-18 12:18:34 -0300

    added product/create endpoint

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { CreateProductDTO } from './dto/product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  @Post('/create')
+  createProduct(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+    console.log(createProductDTO);
+    return res.status(HttpStatus.OK).json({
+      msg: 'Product created successfully',
+    });
+  }
+}
