12ca6f57abe5a0fed32b02fd84d6401c85d4417a
Agustin Gomez <agusleongomez@gmail.com>
2022-11-21 16:28:23 -0300

    added enpoints

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,13 +1,69 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { CreateProductDTO } from './dto/product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-  @Post('/create')
-  createProduct(@Res() res, @Body() createProductDTO: CreateProductDTO) {
-    // console.log(createProductDTO);
+  constructor(private productService: ProductService) {}
+
+  @Get('/')
+  async getProducts(@Res() res) {
+    const products = await this.productService.getProducts();
+    return res.status(HttpStatus.OK).json({ products });
+  }
+
+  @Get('/:productID')
+  async getProduct(@Res() res, @Param('productID') productID) {
+    const product = await this.productService.getProduct(productID);
+    if (!product) throw new NotFoundException('Product does not exist');
+    return res.status(HttpStatus.OK).json(product);
+  }
+
+  @Post('create')
+  async createProduct(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+    const product = await this.productService.createProduct(createProductDTO);
     return res.status(HttpStatus.OK).json({
-      msg: 'Product created successfully',
+      msg: 'Product Successfully Created',
+      product,
+    });
+  }
+
+  @Put('/update')
+  async updateProduct(
+    @Res() res,
+    @Body() createProductDTO: CreateProductDTO,
+    @Query('productID') productID,
+  ) {
+    const productUpdated = await this.productService.updateProduct(
+      productID,
+      createProductDTO,
+    );
+    if (!productUpdated) throw new NotFoundException('Product does not exist');
+    return res.status(HttpStatus.OK).json({
+      msg: 'Product Updated Successfully',
+      productUpdated,
+    });
+  }
+
+  @Delete('/delete')
+  async deleteProduct(@Res() res, @Query('productID') productID) {
+    const productDeleted = await this.productService.deleteProduct(productID);
+    if (!productDeleted) throw new NotFoundException('Product does not exist');
+    return res.status(HttpStatus.OK).json({
+      msg: 'Product Deleted Successfully',
+      productDeleted,
     });
   }
 }
